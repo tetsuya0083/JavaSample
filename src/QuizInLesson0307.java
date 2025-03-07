5c0c924fee6c949d6c79c007db9b5038bb471c0a
tetsuya <cinemamyg@gmail.com>
2025-03-07 01:49:36 -0500

    fixed some warning

--- src/QuizInLesson0307.java
+++ src/QuizInLesson0307.java
@@ -5,7 +5,7 @@ public class QuizInLesson0307 {
     public static void main(String[] args) {
         //"aaaabbccd" 문자열 한 글자씩 출력하기.
         StringBuilder sb1 = new StringBuilder("aaaabbccd");
-        System.out.println(sb1.toString());
+        System.out.println(sb1);
         for (int i = 0; i < sb1.length(); i++){
             System.out.println(sb1.charAt(i));
         }
@@ -36,7 +36,7 @@ public class QuizInLesson0307 {
         for (Map.Entry<Character, Integer> entry : charNumMap.entrySet()){
             sb4.append(entry.getKey()).append(entry.getValue());
         }
-        System.out.println(sb4.toString());
+        System.out.println(sb4);
 
 
     }
